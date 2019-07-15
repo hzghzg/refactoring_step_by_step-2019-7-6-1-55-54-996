30f97efb06cf5cf9bfc49f9a37464ae7d36e6a5b
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-15 15:48:44 +0800

    Refactor:Extract legal age 18 to a CONSTANT

--- src/main/java/com/tws/refactoring/Driver.java
+++ src/main/java/com/tws/refactoring/Driver.java
@@ -1,6 +1,7 @@
 package com.tws.refactoring;
 
 public class Driver {
+    public static final int LEGAL_Age = 18;
     private int age;
 
     public Driver() {
@@ -10,15 +11,15 @@ public class Driver {
         this.age = age;
     }
 
-    public static boolean isLegal(Driver driver) {
-        return driver.age>= 18;
-    }
-
     public void setAge(int age) {
         this.age = age;
     }
 
     public int getAge() {
         return age;
+    }
+
+    public static boolean isLegal(Driver driver) {
+        return driver.age>=LEGAL_Age;
     }
 }
